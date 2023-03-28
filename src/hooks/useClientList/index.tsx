4dcaa4ccac54f29281183fed04b14b16649aef54
Gustavo Bertonsin <gustavobertonsin@hotmail.com>
2023-03-28 10:51:01 -0300

    feat: :goal_net: Adicionando tratamento de erros
    Foi adicionado o tratamento de erros ao react-query

--- src/hooks/useClientList/index.tsx
+++ src/hooks/useClientList/index.tsx
@@ -1,15 +1,27 @@
+import { useToast } from '@chakra-ui/react';
 import { useQuery } from '@tanstack/react-query';
 import axios from 'axios';
 import { ClientItemProps } from '../../pages/newOrder/components/clientList/clientItem/Types/ClientItemProps';
 
 export default function useClientList() {
-  const { data, isLoading, error } = useQuery<ClientItemProps[]>({
+  const toastError = useToast();
+
+  const { data, isLoading, isError } = useQuery<ClientItemProps[]>({
     queryKey: ['clientList'],
     queryFn: async () => {
       const response = await axios.get('http://localhost:3000/clients');
       return response.data;
     },
+    onError: (error) =>
+      toastError({
+        description: `Ocorreu um erro: ${error}`,
+        status: 'error',
+        position: 'top-right',
+        duration: 9000,
+        variant: 'subtle',
+        isClosable: true,
+      }),
   });
 
-  return { data, isLoading, error };
+  return { data, isLoading, isError };
 }
--- src/hooks/useItemList/index.tsx
+++ src/hooks/useItemList/index.tsx
@@ -1,15 +1,26 @@
+import { useToast } from '@chakra-ui/react';
 import { useQuery } from '@tanstack/react-query';
 import axios from 'axios';
 import { ItemProps } from '../../pages/newOrder/components/itemList/item/Types/ItemProps';
 
 export default function useItemList() {
-  const { data, isLoading, error } = useQuery<ItemProps[]>({
+  const toastError = useToast();
+  const { data, isLoading, isError } = useQuery<ItemProps[]>({
     queryKey: ['ItemList'],
     queryFn: async () => {
       const response = await axios.get('http://localhost:3000/itens');
       return response.data;
     },
+    onError: (error) =>
+      toastError({
+        description: `Ocorreu um erro: ${error}`,
+        status: 'error',
+        position: 'top-right',
+        duration: 9000,
+        variant: 'subtle',
+        isClosable: true,
+      }),
   });
 
-  return { data, isLoading, error };
+  return { data, isLoading, isError };
 }
